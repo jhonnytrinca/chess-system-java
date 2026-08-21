a441a43f3f32440c7b5239fac3ab32dfd38672d3
JhonnyTrinca <jhonny.trinca@hotmail.com>
2026-08-21 10:04:31 -0300

    movimentos possíveis para as peças

--- src/main/java/com/jtrinca/cursos/chess/ChessPiece.java
+++ src/main/java/com/jtrinca/cursos/chess/ChessPiece.java
@@ -3,9 +3,9 @@ package com.jtrinca.cursos.chess;
 import com.jtrinca.cursos.boardgame.Board;
 import com.jtrinca.cursos.boardgame.Piece;
 
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
 
-    private Color color;
+    private final Color color;
 
     public ChessPiece(Board board, Color color) {
         super(board);
